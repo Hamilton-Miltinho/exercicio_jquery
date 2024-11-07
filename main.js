f8e1043feea3d42ad9b8fc96d04e5ae49080fa22
Hamilton <hamiltonnjunior@gmail.com>
2024-11-07 18:47:47 -0300

    atualização

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ $(document).ready(function(){
         e.preventDefault();
 
         const novaTarefa = $('#nome-tarefa').val();
-        const novoItem = $(`<p class="overlay" style="display: none;">${novaTarefa}</p>`);
-        $(novoItem).appendTo('div');
+        const novoItem = $(`<li style="display: none;">${novaTarefa}</li>`);
+        $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
         $('#nome-tarefa').val('');
         
-        $('.overlay').click(function(){
+        $('li').click(function(event) {
+        // $('.overlay').click(function(event) {
             $(this).toggleClass('line-through');
+            console.log(`A classe 'line-through' foi ${$(this).hasClass('line-through') ? 'adicionada' : 'removida'}`);
+            // console.log(this);
+            // $(this).addClass('line-through');
+            event.stopPropagation();
         })
     })
 })
